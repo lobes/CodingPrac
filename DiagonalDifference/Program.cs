9fb055b8c95feffb4bc45ffa1e936ef24e9f0b6e
Sam Giles <samuel.t.giles@gmail.com>
2022-07-09 12:14:18 +1000

    Add DiagonalDifference

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,18 @@
+﻿var matrix = new List<List<int>>
+{
+    new List<int> { 1, 2, 3 },
+    new List<int> { 4, 5, 6 },
+    new List<int> { 9, 8, 9 }
+};
+
+var leftDiagonal = 0;
+var rightDiagonal = 0;
+
+for (var row = 0; row < matrix.Count(); row++)
+{
+    leftDiagonal += matrix[row][row];
+    rightDiagonal += matrix[row][matrix.Count() - 1 - row];
+}
+
+// expected output is |15 - 17| = 2
+Console.WriteLine(Math.Abs(rightDiagonal - leftDiagonal));
